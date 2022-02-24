271bc741348b4f5cd946f19a0a1519fb14624ac1
Richard Chung <80141657+gregwoodsfrontier@users.noreply.github.com>
2022-02-24 01:19:14 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,119 @@
+const listContainer = document.querySelector('[data-lists]')
+const newListForm = document.querySelector('[data-new-list-form]')
+const newListInput = document.querySelector('[data-new-list-input]')
+const deleteListButton = document.querySelector('[data-delete-list-button]')
+const listDisplayContainer = document.querySelector('[data-list-display-container]')
+const listTitleElement = document.querySelector('[data-list-title]')
+const listCountElement = document.querySelector('[data-list-count]')
+const taskContainer = document.querySelector('[data-tasks]')
+const taskTemplate = document.getElementById('task-template')
+
+const LOCAL_STORAGE_LIST_KEY = 'task.lists'
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
+
+deleteListButton.addEventListener('click', e => {
+  lists = lists.filter(list => list.id !== selectedListId)
+  selectedListId = null
+  saveAndRender()
+})
+
+listContainer.addEventListener('click', e => {
+  if(e.target.tagName.toLowerCase() === 'li') {
+    selectedListId = e.target.dataset.listId
+    saveAndRender();
+  }
+})
+
+newListForm.addEventListener('submit', e => {
+  e.preventDefault()
+  const listName = newListInput.value
+  if (listName == null || listName === '') return
+  const list = createList(listName)
+  newListInput.value = null
+  lists.push(list)
+  saveAndRender()
+})
+
+function createList(_name) {
+  return {
+    id: Date.now().toString(),
+    name: _name,
+    tasks: [{
+      id: '12sss',
+      name: 'test',
+      complete: false
+    }]
+  }
+}
+
+function saveAndRender() {
+  save()
+  render()
+}
+
+function save() {
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+  localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
+}
+
+function render() {
+  clearElement(listContainer)
+  renderLists()
+
+  const selectedList = lists.find(list => list.id === selectedListId)
+
+  if (selectedListId == null) {
+    listDisplayContainer.style.display = 'none'
+  } else {
+    listDisplayContainer.style.display = ''
+    listTitleElement.innerText = selectedList.name
+    renderTaskCount(selectedList)
+    clearElement(taskContainer)
+    renderTasks(selectedList)
+  }
+}
+
+function renderTasks(_selectedList) {
+  console.log('render Task')
+  _selectedList.tasks.forEach(task => {
+    const taskElement = document.importNode(taskTemplate.content, true)
+    const checkbox = taskElement.querySelector('input')
+    checkbox.id = task.id
+    checkbox.checked = task.complete
+    const label = taskElement.querySelector('label')
+    label.htmlFor = task.id
+    label.append(task.name)
+    taskContainer.appendChild(taskElement)
+  })
+}
+
+function renderTaskCount(_selectedList) {
+  const incompleteTaskCount = _selectedList.tasks.filter(task => !task.complete).length
+  const taskString = incompleteTaskCount === 1 ? "task" : "tasks"
+  listCountElement.innerText = `${incompleteTaskCount} ${taskString} remaining`
+}
+
+function renderLists() {
+  lists.forEach(list => {
+    const listElement = document.createElement('li')
+    listElement.dataset.listId = list.id
+    listElement.classList.add("list-name")
+    listElement.innerText = list.name
+
+    if(list.id === selectedListId) {
+      listElement.classList.add('active-list')
+    }
+
+    listContainer.appendChild(listElement)
+  })
+}
+
+function clearElement(element) {
+  while(element.firstChild) {
+    element.removeChild(element.firstChild)
+  }
+}
+
+render()
